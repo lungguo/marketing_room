86f3da3f07d505aae21c24c808a2913842770c11
lungguo <137847841+lungguo@users.noreply.github.com>
2023-06-27 09:59:36 +0800

    Create prices.php

---
+++ prices.php
@@ -0,0 +1,17 @@
+    <div class="desktop-padding desktop-flex">
+        <div class="sprites topico1"></div>
+        <div class="sprites topico2"></div>
+        <div class="sprites topico3 best-img"></div>
+        <div class="sprites topico4"></div>
+        <div class="sprites topico5"></div>
+        <!-- <img class="private-room-img" src="https://www.pebblebrookseniorhomecare.com/1Private.svg" alt="private room text" height="35" width="172">
+        <img class="review-img" src="https://www.pebblebrookseniorhomecare.com/review1.svg" alt="review text" width="201" height="35" onclick="location.href='https://www.google.com/maps/place/Pebble+Brook+Senior+Assisted+Living/@33.4868099,-117.0733413,17z/data=!3m1!4b1!4m5!3m4!1s0x80db78a9c85a16cf:0xab889f9d2ade632!8m2!3d33.4868099!4d-117.0711526?hl=en-US'">
+        <img class="best-img" src="https://www.pebblebrookseniorhomecare.com/best.png" alt="best text" width="40" height="16">
+        <img class="month-img" src="https://www.pebblebrookseniorhomecare.com/4500.svg" alt="month text" width="172" height="34">
+        <img class="lisence-img" src="https://www.pebblebrookseniorhomecare.com/License.svg" alt="lisence text" width="134" height="29"> -->
+    </div>
+    <div class="destop-contactus-img-container">
+        <a href="tel:951-303-0253">
+            <img class="contact-us-img" src="https://www.pebblebrookseniorhomecare.com/contact.svg" alt="contact us button" width="191" height="57">
+        </a>
+    </div>
