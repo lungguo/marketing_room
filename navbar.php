ebf365217e7156e36a744bac94779b964fc6beb5
lungguo <137847841+lungguo@users.noreply.github.com>
2023-06-27 09:59:17 +0800

    Create navbar.php

---
+++ navbar.php
@@ -0,0 +1,263 @@
+
+<header id="navbar">
+
+    <div class="header-wrapper" >
+            <div class="top-header-wrapper section-desktop" style="box-shadow: unset">
+                <div class="top-header">
+                    <div style="align-self: center;">
+                        <span class="iconify" data-icon="carbon:location-filled"></span>
+                        <span style="color: #fff">33722 Pebble Brook Circle</span>
+                    </div>
+                    <div style="align-self: center;">
+                        <span class="iconify" data-icon="ant-design:phone-filled"></span>
+                        <a href="tel:9513030253" style="color:#fff;text-decoration: none;"><span style="color: #fff">(951) 303-0253</span></a>
+                    </div>
+                    <div style="align-self: center;">
+                        <span class="iconify" data-icon="ant-design:mail-outlined"></span>
+                        <a href="mailto:pbshomecare@gmail.com" style="color:#fff;text-decoration: none;"><span style="color: #fff">info@pbshomecare.com</span></a>
+                    </div>
+                </div>
+            </div>
+
+        <div class="header-content">
+
+            <div class="title" id="title1" style="color:#FFCCFF;text-align: center;margin-left: auto;margin-right: auto">
+                <a href="https://pebblebrookseniorhomecare.com"><img src="https://www.pebblebrookseniorhomecare.com/logo4.svg" style ="height: 90px" alt="logo" class="mobile-only" /></a>
+            </div>
+            <div class="header-container">
+                <nav style="display: inline-block;">
+                    <div class="menu-overlay" ></div>
+                    <label for="drop" class="toggle" style="border:none;display: inline-block;">
+                    <!--
+                    <span class="bg-menu">
+                    <span></span>
+                    <span></span>
+                    <span></span></span>      
+                    -->
+                    <!--  <span class="iconify" data-icon="bxs:down-arrow">Menu</span>-->
+                        <span class="menu-btn">MENU  <span class="down-icon"></span> </span>
+                    </label>
+                    <input type="checkbox" id="drop" />
+                    <ul class="menu main-ul">
+                        <li class="pg_close_li">
+                            <label for="drop" class="toggle"><span class="pg_close"></span></label>
+                        </li>    
+                        <li class="display-flex">
+                            <img src="https://api.iconify.design/akar-icons/home.svg?color=%23999" class="sidebar-icon" alt=" home icon"/>
+                            <a href="https://pebblebrookseniorhomecare.com" class="stretched-link">Home</a>
+                        </li>
+                        <li class="display-flex">
+                            <img src="https://api.iconify.design/ant-design/camera-outlined.svg?color=%23999" class="sidebar-icon" alt="tour icon"/>
+                            <a href="https://pebblebrookseniorhomecare.com/gallery/" class="stretched-link">Virtual Tour</a>
+                        </li>
+                        <li class="display-flex">
+                            <img src="https://api.iconify.design/ant-design/calendar-twotone.svg?color=%23999" class="sidebar-icon" alt="event icon"/>
+                            <a href="https://pebblebrookseniorhomecare.com/events/" class="stretched-link">Events</a>
+                        </li>
+                        <li class="display-flex">
+                            <img src="https://api.iconify.design/akar-icons/star.svg?color=%23999" class="sidebar-icon" alt="Testimonials icon"/>
+                            <a href="https://www.google.com/search?q=Pebble+Brook+Senior+home+care&oq=peb&aqs=chrome.1.69i60j69i59l2j69i57j69i60l4.2036j0j7&sourceid=chrome&ie=UTF-8#lrd=0x80db78a9c85a16cf:0xab889f9d2ade632,1,,," rel="nofollow" target="_blank" class="stretched-link">Testimonials</a>
+                        </li>
+                        <li class="display-flex">
+                            <img src="https://api.iconify.design/carbon/email.svg?color=%23999" class="sidebar-icon" alt="contact us icon"/>
+                            <a href="https://pebblebrookseniorhomecare.com/contact/" class="stretched-link">Contact us</a>
+                        </li>
+                        <li class="our-services"><!-- Domain -->
+                            <div class="display-flex">
+                                <img src="https://api.iconify.design/akar-icons/person.svg?color=%23999" class="sidebar-icon" alt="user icon"/>
+                                <label for="drop-1" class="toggle services-toggle">
+                                    Our Services
+                                    <img src="https://api.iconify.design/dashicons/arrow-right.svg?color=black" alt="arrow icon" class="right-arrow" />
+                                    <img src="https://api.iconify.design/dashicons/arrow-down.svg?color=black" alt="arrow icon" class="d-none down-arrow"/>
+                                </label>
+                            </div>
+                            <input type="checkbox" id="drop-1"/>
+                            <ul>
+                                <li><a href="https://pebblebrookseniorhomecare.com/basics/" class="stretched-link">The Basics</a></li>
+                                <li><a href="https://pebblebrookseniorhomecare.com/dining/" class="stretched-link">Dining</a></li>
+                                <li><a href="https://pebblebrookseniorhomecare.com/hospice/" class="stretched-link">Hospice Service</a></li>
+                                <li><a href="https://pebblebrookseniorhomecare.com/transportation/" class="stretched-link">Transportation</a></li>
+                                <li><a href="https://pebblebrookseniorhomecare.com/3rdpartyservices/" class="stretched-link">Additional Services</a></li>
+                            </ul> 
+                        </li><!-- Gender Tier Drop End -->
+
+                        <li class="facility-features"> <!-- Domain --> 
+
+                            <div class="display-flex">
+                                <img src="https://api.iconify.design/bx/wifi-2.svg?color=%23999" class="sidebar-icon" alt="wifi icon"/>
+                                <label for="drop-2" class="toggle facility-toggle">
+
+                                    Facility Features
+
+                                    <img src="https://api.iconify.design/dashicons/arrow-right.svg?color=black" alt="arrow icon" class="right-arrow" />
+                                    <img src="https://api.iconify.design/dashicons/arrow-down.svg?color=black" alt="arrow icon" class="d-none down-arrow"/>
+
+                                </label>
+                            </div>
+                            <input type="checkbox" id="drop-2"/>
+                            <ul>
+                                <li><a href="https://pebblebrookseniorhomecare.com/equipments/" class="stretched-link">Equipment List</a></li>
+                                <li><a href="https://pebblebrookseniorhomecare.com/rooms/" class="stretched-link">Spacious Rooms and Bathrooms</a></li>
+                                <li><a href="https://pebblebrookseniorhomecare.com/wifi/" class="stretched-link">TV, WIFI, Decor Included</a></li>
+                                <li><a href="https://pebblebrookseniorhomecare.com/safety/" class="stretched-link">Safety</a></li>
+
+                            </ul> 
+                        </li>
+                        <li class="display-flex">
+                            <img src="https://api.iconify.design/majesticons/covid-line.svg?color=%23999" class="sidebar-icon" alt="contact us icon"/>
+                            <a href="https://pebblebrookseniorhomecare.com/covid19/" class="stretched-link">COVID19 Response</a>
+                        </li>
+                    </ul>
+                </nav>
+                <div class=icon-container>
+                    <a class = "icons twitter-icon" href="https://twitter.com/pbshomecare" aria-label="Twitter" rel="nofollow" style="padding: 23px;vertical-align: middle;"></a>
+                    <a class = "icons fb-icon" href="https://www.facebook.com/pbseniorhomecare/" aria-label="Facebook" rel="nofollow"></a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="header-wrapper header-wrapper-fixed">
+        <div class="header-content">
+            <div class="flex-container">
+            <!-- Green background for mobile only -->
+                <nav class="view-mobile-only">
+                    <div class="menu-overlay"></div>
+                    <label for="drop" class="toggle">
+                        <span class="bg-menu" style="">
+                            <span></span>
+                            <span></span>
+                            <span></span>                        
+                        </span>                    
+                    </label>
+                </nav>
+                <a class="text-decoration-none  ms-3 link-container view-mobile-only" href="tel:951-303-0253"> 
+                    <div class="title" id="title2" style="padding-top: 33px;text-align: left">(951) 303 0253</div>
+                </a>                
+                <a class="view-mobile-only map" href="https://www.google.com/maps/dir//Pebble+Brook+Senior+Assisted+Living,+33722+Pebble+Brook+Cir,+Temecula,+CA+92592/@33.5100485,-117.1636795,13z/data=!4m9!4m8!1m0!1m5!1m1!1s0x80db78a9c85a16cf:0xab889f9d2ade632!2m2!1d-117.0711517!2d33.4868066!3e0" target="new_window">
+                    <img src="https://api.iconify.design/ph/map-pin-fill.svg?color=white" class="sidebar-icon" alt="location icon" width="25" height="25">
+                </a>
+
+                <!-- Sticky header -->
+
+                <div class="header-container view-desktop-only" style="width: 100%;flex-grow: 1;justify-content: space-between;">
+                    <nav style="display: inline-block;flex:unset ">
+                        <div class="menu-overlay"></div>
+                        <label for="mob-drop" class="toggle" style="border:none;display: inline-block;">
+                            <span class="menu-btn" style="padding-right: 20px;">MENU  <span class="down-icon" style="top: 40px;left: 43px"></span></span>
+                        </label>
+                        <input type="checkbox" id="mob-drop">
+                        <ul class="menu main-ul">
+                            <li class="pg_close_li">
+                                <label for="drop" class="toggle"><span class="pg_close" data-ol-has-click-handler=""></span></label>
+                            </li>    
+                            <li class="display-flex">
+                                <img src="https://api.iconify.design/akar-icons/home.svg?color=%23999" class="sidebar-icon" alt=" home icon">
+                                <a href="https://pebblebrookseniorhomecare.com" class="stretched-link">Home</a>
+                            </li>
+                            <li class="display-flex">
+                                <img src="https://api.iconify.design/ant-design/camera-outlined.svg?color=%23999" class="sidebar-icon" alt="tour icon">
+                                <a href="https://pebblebrookseniorhomecare.com/gallery/" class="stretched-link">Virtual Tour</a>
+                            </li>
+                            <li class="display-flex">
+                                <img src="https://api.iconify.design/ant-design/calendar-twotone.svg?color=%23999" class="sidebar-icon" alt="event icon">
+                                <a href="https://pebblebrookseniorhomecare.com/events/" class="stretched-link">Events</a>
+                            </li>
+                            <li class="display-flex">
+                                <img src="https://api.iconify.design/akar-icons/star.svg?color=%23999" class="sidebar-icon" alt="Testimonials icon">
+                                <a href="https://www.google.com/search?q=Pebble+Brook+Senior+home+care&amp;oq=peb&amp;aqs=chrome.1.69i60j69i59l2j69i57j69i60l4.2036j0j7&amp;sourceid=chrome&amp;ie=UTF-8#lrd=0x80db78a9c85a16cf:0xab889f9d2ade632,1,,," rel="nofollow" target="_blank" class="stretched-link">Testimonials</a>
+                            </li>
+                            <li class="display-flex">
+                                <img src="https://api.iconify.design/carbon/email.svg?color=%23999" class="sidebar-icon" alt="contact us icon">
+                                <a href="https://pebblebrookseniorhomecare.com/contact/" class="stretched-link">Contact us</a>
+                            </li>
+                            <li class="our-services"><!-- Domain -->
+                                <div class="display-flex">
+                                    <img src="https://api.iconify.design/akar-icons/person.svg?color=%23999" class="sidebar-icon" alt="user icon">
+                                    <label for="drop-1" class="toggle services-toggle" data-ol-has-click-handler="">
+                                        Our Services
+
+                                        <img src="https://api.iconify.design/dashicons/arrow-right.svg?color=black" alt="arrow icon" class="right-arrow">
+                                        <img src="https://api.iconify.design/dashicons/arrow-down.svg?color=black" alt="arrow icon" class="d-none down-arrow">
+
+                                    </label>
+                                </div>
+                                <input type="checkbox" id="mob-drop-1">
+                                <ul>
+                                    <li><a href="https://pebblebrookseniorhomecare.com/basics/" class="stretched-link">The Basics</a></li>
+                                    <li><a href="https://pebblebrookseniorhomecare.com/dining/" class="stretched-link">Dining</a></li>
+                                    <li><a href="https://pebblebrookseniorhomecare.com/hospice/" class="stretched-link">Hospice Service</a></li>
+                                    <li><a href="https://pebblebrookseniorhomecare.com/transportation/" class="stretched-link">Transportation</a></li>
+                                    <li><a href="https://pebblebrookseniorhomecare.com/3rdpartyservices/" class="stretched-link">Additional Services</a></li>
+                                </ul> 
+                            </li><!-- Gender Tier Drop End -->
+
+                            <li class="facility-features"> <!-- Domain --> 
+                                <div class="display-flex">
+                                    <img src="https://api.iconify.design/bx/wifi-2.svg?color=%23999" class="sidebar-icon" alt="wifi icon">
+                                    <label for="drop-2" class="toggle facility-toggle" data-ol-has-click-handler="">
+                                        Facility Features
+                                        <img src="https://api.iconify.design/dashicons/arrow-right.svg?color=black" alt="arrow icon" class="right-arrow">
+                                        <img src="https://api.iconify.design/dashicons/arrow-down.svg?color=black" alt="arrow icon" class="d-none down-arrow">
+                                    </label>
+                                </div>
+                                <input type="checkbox" id="mob-drop-2">
+                                <ul>
+                                    <li><a href="https://pebblebrookseniorhomecare.com/equipments/" class="stretched-link">Equipment List</a></li>
+                                    <li><a href="https://pebblebrookseniorhomecare.com/rooms/" class="stretched-link">Spacious Rooms and Bathrooms</a></li>
+                                    <li><a href="https://pebblebrookseniorhomecare.com/wifi/" class="stretched-link">TV, WIFI, Decor Included</a></li>
+                                    <li><a href="https://pebblebrookseniorhomecare.com/safety/" class="stretched-link">Safety</a></li>
+
+                                </ul> 
+                            </li>
+                            <li class="display-flex">
+                                <img src="https://api.iconify.design/majesticons/covid-line.svg?color=%23999" class="sidebar-icon" alt="contact us icon">
+                                <a href="https://pebblebrookseniorhomecare.com/covid19/" class="stretched-link">COVID19 Response</a>
+                            </li>
+                        </ul>
+                    </nav>
+                    <h2 style="color: #fff">Pebble Brook Assisted Living</h2>
+                    <div class="icon-container" style="align-self: center">
+                        <a class="icons twitter-icon" href="https://twitter.com/pbshomecare" rel="nofollow" style="padding: 23px;vertical-align: middle;"></a>
+                        <a class="icons fb-icon" href="https://www.facebook.com/pbseniorhomecare/" target="nofollow"></a>
+                    </div>
+                </div>
+
+            <!-- sticky for menu for desktop -->
+
+                <div class="section-desktop view-desktop-only" style="width: 100%;flex-grow: 1">
+                    <div class="desktop-padding desktop-flex">
+                        <div class="sprites topico1"></div>
+                        <div class="sprites topico2"></div>
+                        <div class="sprites topico3 best-img"></div>
+                        <div class="sprites topico4"></div>
+                        <div class="sprites topico5"></div>
+                        <!-- <img class="private-room-img" src="https://www.pebblebrookseniorhomecare.com/1Private.svg" alt="private room text" height="35" width="172">
+                        <img class="review-img" src="https://www.pebblebrookseniorhomecare.com/review1.svg" alt="review text" width="201" height="35" onclick="location.href='https://www.google.com/maps/place/Pebble+Brook+Senior+Assisted+Living/@33.4868099,-117.0733413,17z/data=!3m1!4b1!4m5!3m4!1s0x80db78a9c85a16cf:0xab889f9d2ade632!8m2!3d33.4868099!4d-117.0711526?hl=en-US'">
+                        <img class="best-img" src="https://www.pebblebrookseniorhomecare.com/best.png" alt="best text" width="40" height="16">
+                        <img class="month-img" src="https://www.pebblebrookseniorhomecare.com/4500.svg" alt="month text" width="172" height="34">
+                        <img class="lisence-img" src="https://www.pebblebrookseniorhomecare.com/License.svg" alt="lisence text" width="134" height="29"> -->
+                    </div>
+                    <div class="destop-contactus-img-container">
+                        <a href="tel:951-303-0253">
+                            <img class="contact-us-img" src="https://www.pebblebrookseniorhomecare.com/contact.svg" alt="contact us button" width="191" height="57">
+                        </a>
+                    </div>
+                </div>    
+            </div>
+        </div>
+    </div>
+</header>
+<script>
+    if($(window).width()>1200){
+        $('.menu.main-ul').css('display',"block")
+    }
+    $(window).on('resize', function(){
+        var win = $(this); //this = window
+        if (win.width() < 1200) {
+            $('.menu.main-ul').css('display',"none")
+        } else {
+            $('.menu.main-ul').css('display',"block")
+        }
+    });    
+</script>
