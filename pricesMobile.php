bfd14c7b0309e36998aa08bdedbd5ed58568f913
lungguo <137847841+lungguo@users.noreply.github.com>
2023-06-27 09:59:58 +0800

    Create pricesMobile.php

---
+++ pricesMobile.php
@@ -0,0 +1,26 @@
+    <div class="flex-view">
+        <div class="sprites topico1"></div>
+        <div class="sprites topico2"></div>
+        <!-- <img class="private-room-img" src="https://www.pebblebrookseniorhomecare.com/1Private.svg" alt="private room price" width="132" height="27">
+        <img class="month-img" src="https://www.pebblebrookseniorhomecare.com/4500.svg" alt="$4,500 monthly cost" width="132" height="27"> -->
+    </div>
+    <div class="flex-view margin-space">
+        
+        <div class="sprites topico3 best-img"></div>
+        <div class="sprites topico4"></div>
+        <div class="sprites topico5"></div>
+        <!-- <img class="review-img" src="https://www.pebblebrookseniorhomecare.com/review1.svg" alt="read our google reviews" onclick="location.href='https://www.google.com/maps/place/Pebble+Brook+Senior+Assisted+Living/@33.4868099,-117.0733413,17z/data=!3m1!4b1!4m5!3m4!1s0x80db78a9c85a16cf:0xab889f9d2ade632!8m2!3d33.4868099!4d-117.0711526?hl=en-US'" width="144" height="28">
+        <img class="lisence-img" src="https://www.pebblebrookseniorhomecare.com/License.svg" alt="state license #" width="129" height="28">
+        <img class="best-img" src="https://www.pebblebrookseniorhomecare.com/best.png" alt="best rated" width="40" height="16"> -->
+    </div>
+    <div class="tablet-flex">
+        <div class="sprites topico1"></div>
+        <div class="sprites topico2"></div>
+        <div class="sprites topico3 best-img"></div>
+        <div class="sprites topico5"></div>
+    </div>
+    <div class="contact-us-container">
+        <a href="tel:951-303-0253">
+            <img class="contact-us-img" src="https://www.pebblebrookseniorhomecare.com/contact.svg" alt="contact us button" width="137" height="41">
+        </a>
+    </div>
